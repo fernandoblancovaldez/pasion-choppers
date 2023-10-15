adce830eecef9fd2eeb5cca5cfd7c2d2cf834e3f
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-10-15 05:09:54 -0300

    1st commit, starting the structure

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Chat from "./Chat/Chat";
+import Slider from "./Slider/Slider";
+
+const Main = () => {
+  return (
+    <main>
+      contenido central
+      <Slider />
+      <Chat />
+    </main>
+  );
+};
+
+export default Main;
